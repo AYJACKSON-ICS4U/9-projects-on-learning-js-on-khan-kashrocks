2056d8748ee374498eda3705efa8353beaa6859d
kashrocks <31697626+kashrocks@users.noreply.github.com>
2017-11-03 11:21:59 -0400

    Create 1_Challenge_Sunny.Snowy.day.js

---
+++ 1_Challenge_Sunny.Snowy.day.js
@@ -0,0 +1,15 @@
+// The ground
+background(0, 26, 255);
+fill(0, 255, 13);
+rect(0, 300, 400, 100);  
+
+// The sun
+fill(255, 230, 0);
+ellipse(80, 64, 100, 100);  
+
+// The snowman
+fill(255, 255, 255);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
+
