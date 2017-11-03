7b5e5a8c7557a277f4d6461b579411e5ee4ed063
kashrocks <31697626+kashrocks@users.noreply.github.com>
2017-11-03 11:33:47 -0400

    Create 6_Challenge_say.your.name.js

---
+++ 6_Challenge_say.your.name.js
@@ -0,0 +1,31 @@
+var yourName = "Kashyap";
+
+var Name = function() {
+    var textX = random(0, 300);
+var textY = random(0, 300);
+ fill(255, 0, 0);
+textSize(30);
+text("Hiiii, " + yourName, textX, textY);   
+    
+};
+
+Name();
+Name();
+
+Name();
+
+Name();
+
+Name();
+
+Name();
+
+Name();
+
+Name();
+Name();
+Name();
+
+Name();
+
+Name();
