a78b41a6ced3f9ad9ded37167f79e2b17a02597f
kashrocks <31697626+kashrocks@users.noreply.github.com>
2017-11-08 13:29:23 -0500

    Update 9_Challenge_movie.review.js

--- 9_Challenge_movie.review.js
+++ 9_Challenge_movie.review.js
@@ -2,7 +2,7 @@ var movies = [
     {
         title: "Puff the Magic Dragon",
         review: "Best movie ever!!"
-    },
+    }, 
     {
         title: "HOW TO TRAIN YOUR DRAGON",
         review: "GREATEST movie ever!!"   
@@ -11,12 +11,11 @@ var movies = [
 
 fill(84, 140, 209);
 textAlign(CENTER, CENTER);
-textSize(20);
-text(movies[0].title, 200, 50);
-textSize(15);
-text(movies[0].review, 200, 75);
 
-textSize(20);
-text(movies[1].title, 200, 100);
-textSize(15);
-text(movies[1].review, 200, 125);
+for (var x = 0; x < movies.length; x++){
+    textSize(20);
+    text(movies[x].title, 200, 50 + (x*50));
+    textSize(15);
+    text(movies[x].review, 200, 75 + (x*50));
+}
+
